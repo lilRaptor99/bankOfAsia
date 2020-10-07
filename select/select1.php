21dac76f7ed8ba58f420bdbf0778cf5eb2865b31
Pratheek Senevirathne <pratheeksenevirathne@gmail.com>
2020-10-07 16:23:01 +0530

    user query and result error for select1 query

--- select/select1.php
+++ select/select1.php
@@ -10,20 +10,24 @@
 </head>
 <body>
 
-    <h3>SELECT Queries</h3>
+    <h3>SELECT Query 1</h3>
     <?php 
         $db;
         include("../db_connect.php"); //connects to the Database according to the user level. variable $db will be used.
 
         $sql = "SELECT username, type FROM employee_login WHERE employee_id = '00005';";
         $result = mysqli_query($db,$sql);
-        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
         
-        if ($result) {
+        if ($result) { // query successful
+            $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
             echo "Returned rows are: " . mysqli_num_rows($result);
+            echo $row['username'] . " space " . $row['type'];
             // Free result set
             mysqli_free_result($result);
-          }
+        } else {
+            echo "<p>Error: Invalid Data or you have NO PERMISSION to execute this query</p>";
+            echo "<p>Error description: " . mysqli_error($db) . "</p>";
+        }
           
           mysqli_close($db);
 
