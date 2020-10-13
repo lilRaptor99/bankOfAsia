3fd6b73ec2e66f8ee359201f66c87c5eb3f5d052
Pratheek Senevirathne <pratheeksenevirathne@gmail.com>
2020-10-13 11:51:26 +0530

    db_connect test

--- db_connect.php
+++ db_connect.php
@@ -46,6 +46,6 @@
     $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
     if (mysqli_connect_errno()) {
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        echo "Failed to connect tsdafasdhvkjvgljvhfasdfadso MySQL: " . mysqli_connect_error();
     }
 ?>
